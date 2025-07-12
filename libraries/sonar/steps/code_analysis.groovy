d0a88ee68f025ac9f923f05fff627c04593baa89
kkdevopsb5onlie <dharimigariarjun@gmail.com>
2025-07-12 17:39:33 +0530

    Update code_analysis.groovy

---
+++ libraries/sonar/steps/code_analysis.groovy
@@ -0,0 +1,14 @@
+void call () {
+    println("####### STARTED SONARQUBE ANALYISIS ##############")
+    if(pipelineConfig.SONAR_PROJECT_KEY =! null) {
+      withSonarQubeEnv('SonarQubeServer') {
+        def SONAR_SCANNER_HOME = tool 'SonarScanner' 
+        sh "${SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.projectKey=${pipelineConfig.SONAR_PROJECT_KEY}"
+        println("####### COMPLETED SONARQUBE ANALYISIS ##############")
+
+      }
+    } else {
+      error("Please add SONAR_PROJECT_KEY variable in pipeline_config.groovy file.")
+    }
+}
+  
