53b0adff91333c1333a7d12b38c21178486dc458
kkdevopsb5onlie <dharimigariarjun@gmail.com>
2025-07-18 16:03:07 +0530

    Update deploy.groovy

--- libraries/tomcatDeploy/deploy.groovy
+++ libraries/tomcatDeploy/deploy.groovy
@@ -1,5 +1,5 @@
 void call() {
-   withCredentials([usernamePassword(credentialsId: "${pipelineConfig.TOMCAT-CREDENTIALS}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]){
+   withCredentials([usernamePassword(credentialsId: "${pipelineConfig.TOMCAT_CREDENTIALS}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]){
         sh """
             curl -u ${USERNAME}:${PASSWORD} --upload-file ${pipelineConfig.WAR-FILE-PATH} "http://${pipelineConfig.SERVER_IP_WITH_PORT}/manager/text/deploy?path=/maven-web-application&update=true"
        """
